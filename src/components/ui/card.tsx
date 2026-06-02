0c66998328b5128e713a11ceae1c614fc6b8056d
DavidLima2025 <david1996bh@gmail.com>
2026-06-02 16:13:59 -0300

    Delete src directory

--- src/components/ui/card.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export function Card({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={`${className} rounded-lg border border-slate-200 shadow-sm`} {...props} />;
-}
-
-export function CardContent({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={className} {...props} />;
-}
